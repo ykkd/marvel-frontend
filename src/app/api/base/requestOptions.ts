c61fdcde704c54959e57cd33ae6e667234f62095
ykkd <ykkd221dev@gmail.com>
2025-02-09 16:33:14 +0900

    feat: implement api client base

---
+++ src/app/api/base/requestOptions.ts
@@ -0,0 +1,8 @@
+type RequestOptions = {
+    method?: "GET" | "POST" | "PUT" | "DELETE";
+    headers?: HeadersInit;
+    body?: any;
+    params?: Record<string, string>;
+};
+
+export default RequestOptions;
