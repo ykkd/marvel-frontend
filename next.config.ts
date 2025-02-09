9872f2bea62fcb9e0187ce7ded4dc5fed12ca142
ykkd <ykkd221dev@gmail.com>
2025-02-09 14:56:32 +0900

    chore: add next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  trailingSlash: true,
+  output: "standalone",
 };
 
 export default nextConfig;
